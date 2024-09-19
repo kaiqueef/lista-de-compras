cb118b7cfc75b37d544d0250adbfe043473d2689
Kaique Ferreira <kaiquee.f@hotmail.com>
2024-09-18 21:15:02 -0300

    feat: add PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 import "../styles/global.css";
- 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
