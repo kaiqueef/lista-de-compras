d5b083b28511a65ee1d18d8a11c7154fd4f75e54
Kaique Ferreira <kaiquee.f@hotmail.com>
2024-10-02 15:31:45 -0300

    change icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,26 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#212331" />
+        <meta name="msapplication-TileColor" content="#212331" />
+        <meta name="theme-color" content="#212331" />
       </Head>
       <body>
         <Main />
